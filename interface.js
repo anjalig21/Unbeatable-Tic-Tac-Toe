a8d2cafc5dbb99ab3ee0ce8e0f9432f77b0dc685
anjalig21 <anjaligta@outlook.com>
2021-02-13 17:14:19 -0500

    updated tic-tac-toe algorithm

--- interface.js
+++ interface.js
@@ -4,8 +4,8 @@ let board = [
     ['X', 'O', 'X'],
 ];
 
-let player1 = 'X';
-let player2 = 'O';
+let P1 = 'X';
+let P1 = 'O';
 
 function setup() {
     createCanvas(400, 400);
@@ -13,27 +13,27 @@ function setup() {
 
 function draw() {
     background(255);
-    
-    let w = width / 3;
-    let h = height / 3;
+
+    let w = (width / 3);
+    let h = (height / 3);
 
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
           
-            let x = w * j + w/2;
-            let y = h * i + h/2;
+            let y = (h * i) + (h / 2);
+            let x = (w * j) + (w / 2);
           
             let pos = board[j][i];
             textSize(32);
             
-            if (pos == player1) {
+            if (pos == P1) {
               let size = w/4;
-              line(x-size, y-size, x+size, y+size);
-              line(x+size, y-size, x-size, y+size);
+              line(x - size, y - size, x + size, y + size);
+              line(x + size, y - size, x - size, y + size);
             }
-            else if (pos == player2) {
+            else if (pos == P2) {
               noFill();
-              ellipse(x,y,w/2);
+              ellipse(x,y,(w / 2));
             }
         }
     }
