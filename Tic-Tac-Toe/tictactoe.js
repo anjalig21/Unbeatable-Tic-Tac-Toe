e79a34c5e4d0ff1521bb88a16910b92fb59dd1fe
anjalig21 <anjaligta@outlook.com>
2021-02-13 17:40:08 -0500

    updated styling for board

--- Tic-Tac-Toe/tictactoe.js
+++ Tic-Tac-Toe/tictactoe.js
@@ -1,18 +1,18 @@
 // Initialize Canvas
 function setup() {
     createCanvas(400, 400);
+    currentPlayer = random(players);
 }
 
 // Initialize Board
 let board = [
-    ['X', 'O', 'X'],
-    ['O', 'X', 'O'],
-    ['X', 'O', 'X'],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
 ];
 
 // Initialize Players
-let P1 = 'X';
-let P2 = 'O';
+let players = ['X', 'O'];
 
 // Draws X's and O's relative to the center of a spot on the board
 function draw() {
@@ -20,6 +20,11 @@ function draw() {
 
     let w = (width / 3);
     let h = (height / 3);
+  
+    line(w, 0, w, height);
+    line((2 * w), 0, (2 * w), height);
+    line(0, h, width, h);
+    line(0, (h * 2), width, (h * 2));
 
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
@@ -29,13 +34,14 @@ function draw() {
           
             let pos = board[j][i];
             textSize(32);
+            strokeWeight(3);
             
-            if (pos == P1) {
+            if (pos == players[0]) {
               let size = (w / 4);
               line(x - size, y - size, x + size, y + size);
               line(x + size, y - size, x - size, y + size);
             }
-            else if (pos == P2) {
+            else if (pos == players[1]) {
               noFill();
               ellipse(x,y,(w / 2));
             }
